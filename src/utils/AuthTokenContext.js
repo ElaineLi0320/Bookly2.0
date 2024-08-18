db450c3acda0f97a8b265dac34c60c2253d979ad
Yini Li <104151346+ElaineLi0320@users.noreply.github.com>
2024-08-18 12:36:34 -0700

    simple finish

---
+++ src/utils/AuthTokenContext.js
@@ -0,0 +1,42 @@
+import React, { useContext, useState, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { requestedScopes } from "./constants";
+
+const AuthTokenContext = React.createContext();
+
+function AuthTokenProvider({ children }) {
+  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
+  const [accessToken, setAccessToken] = useState();
+
+  useEffect(() => {
+    const getAccessToken = async () => {
+      try {
+        // get access token silently from Auth0, which will be stored in the context
+        const token = await getAccessTokenSilently({
+          authorizationParams: {
+            audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+            scope: requestedScopes.join(" "),
+          },
+        });
+        setAccessToken(token);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (isAuthenticated) {
+      getAccessToken();
+    }
+  }, [getAccessTokenSilently, isAuthenticated]);
+
+  const value = { accessToken };
+  return (
+    <AuthTokenContext.Provider value={value}>
+      {children}
+    </AuthTokenContext.Provider>
+  );
+}
+
+const useAuthToken = () => useContext(AuthTokenContext);
+
+export { useAuthToken, AuthTokenProvider };
